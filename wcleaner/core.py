250d9355d61d67b30c326f53ef2aa9d8e38ee6ed
Ruiqi Li <liruiqi@wandoujia.com>
2015-02-02 19:00:47 +0800

    headq

--- wcleaner/core.py
+++ wcleaner/core.py
@@ -5,8 +5,9 @@ from wcleaner import __version__
 import os
 import argparse
 import scandir
+import heapq
 
-MAX_CAPACITY = 80
+MAX_CAPACITY = 20
 
 MOUNT_POINTS = {}
 for line in os.popen('df -Plk').readlines()[1:]:
@@ -40,25 +41,30 @@ def wcleaner():
     parser = argparse.ArgumentParser(description='Wandoujia Cleaner')
     parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
     parser.add_argument('FILESYSTEM', type=str, nargs='?', help='the filesystem to clean')
+    parser.add_argument('--max-capacity', type=str, nargs='?', help='the filesystem to clean')
 
     args = parser.parse_args()
 
+    files_info = []
+
     for Point, (Filesystem, Size, Capacity) in MOUNT_POINTS.items():
-        print 'Clean: ', Point, (Filesystem, Size, Capacity)
         #specify the filesystem to clean
-        if args.FILESYSTEM and args.FILESYSTEM != Filesystem: continue
+        if args.FILESYSTEM and args.FILESYSTEM != Filesystem and args.FILESYSTEM != Point: continue
 
         #default to clean >MOUNT_POINTS% filesystem
         if not args.FILESYSTEM and Capacity < MAX_CAPACITY: continue
+
+        print 'Point: ', Point
 
         for i, f in enumerate(walk(Point)):
             stat = f.stat()
             f_size = stat.st_blocks*stat.st_blksize/1024/8
 
-            if f_size / float(Size) > 0.05:
-                print i, f_size, f.path
+            files_info.append((f.path, f_size))
 
-        print '\n'
+    for path, size in heapq.nlargest(10, files_info, key=lambda x: x[1]):
+        print path, size
+    print
 
 if __name__ == '__main__':
     wcleaner()
